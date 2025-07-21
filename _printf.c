d6eca85cc8d4c02d0b2088fbedcf9fa9fc975ba1
JordannM83 <11457@holbertonstudents.com>
2025-07-21 10:43:09 +0200

    Implement _printf function and add format struct in main.h

--- _printf.c
+++ _printf.c
@@ -5,5 +5,25 @@
 
 int _printf(const char *format, ...)
 {
+	va_list args;
+	int len = 0;
+	int i; 
+	
+	print_t format[] = {
+		{"c", fonction},
+		{"s", fonction},
+		{"%", fonction},
+	}
 
+	va_start(args, format);
+
+
+
+
+
+
+
+
+	va_end(args);
+	return(len);
 }
--- main.h
+++ main.h
@@ -1,8 +1,26 @@
 #ifndef _MAIN_
 #define _MAIN_
+#include <stdarg.h>
+
+/**
+ * struct print - Struct print
+ * @c: The format specifier
+ * @f: The function associated
+ *
+ * Description: This struct is used to associate a format specifier
+ * with its corresponding function for printing.
+ */
+
+typedef struct print
+{
+	char *c;
+	void (*f)(va_list);
+} print_t;
 
 int _putchar(char c);
 
 int _printf(const char *format, ...);
+
+
 
 #endif
